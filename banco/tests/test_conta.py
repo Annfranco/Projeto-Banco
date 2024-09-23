a6cf6986ad7022bfaa0100eb4a7c9d8cd1004005
Annfranco <160090372+Annfranco@users.noreply.github.com>
2024-09-23 11:07:37 +0000

    Updade Banco - 16/09

--- banco/tests/test_conta.py
+++ banco/tests/test_conta.py
@@ -12,4 +12,18 @@ def conta_valida():
 
 def test_validando_atributo_numero_conta(conta_valida):
     assert conta_valida.numero_conta == 333
-    
+
+def test_validando_atributo_agencia(conta_valida):
+    assert conta_valida.agencia == 444
+
+def test_validando_atributo_saldo(conta_valida):
+    assert conta_valida._saldo == 0
+
+def test_depositar_valor_positivo(conta_valida):
+    conta_valida.depositar(100)
+    assert conta_valida._saldo == 100
+
+def test_sacar_valor_positivo_com_saldo_suficiente(conta_valida):
+    conta_valida.depositar(100)
+    conta_valida.sacar(100)
+    assert conta_valida._saldo == 0   
